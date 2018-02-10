cd0a5727be2e3dd6c05754fe1b667b64ba5bbc7d
Yassine KLILICH <klilichyassine8@gmail.com>
2018-02-10 22:42:29 +0000

    Change Color 1

---
+++ Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Red = new System.Windows.Forms.TrackBar();
+            this.Green = new System.Windows.Forms.TrackBar();
+            this.Blue = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Color_Change = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Red)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Green)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Blue)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Red
+            // 
+            this.Red.Location = new System.Drawing.Point(12, 379);
+            this.Red.Maximum = 255;
+            this.Red.Name = "Red";
+            this.Red.Size = new System.Drawing.Size(179, 45);
+            this.Red.TabIndex = 0;
+            this.Red.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.Red.Scroll += new System.EventHandler(this.Red_Scroll);
+            // 
+            // Green
+            // 
+            this.Green.Location = new System.Drawing.Point(197, 379);
+            this.Green.Maximum = 255;
+            this.Green.Name = "Green";
+            this.Green.Size = new System.Drawing.Size(179, 45);
+            this.Green.TabIndex = 0;
+            this.Green.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.Green.Scroll += new System.EventHandler(this.Green_Scroll);
+            // 
+            // Blue
+            // 
+            this.Blue.Location = new System.Drawing.Point(392, 379);
+            this.Blue.Maximum = 255;
+            this.Blue.Name = "Blue";
+            this.Blue.Size = new System.Drawing.Size(179, 45);
+            this.Blue.TabIndex = 0;
+            this.Blue.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.Blue.Scroll += new System.EventHandler(this.Blue_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(69, 352);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = " Red";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(252, 352);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = " Green";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(450, 352);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 24);
+            this.label3.TabIndex = 1;
+            this.label3.Text = " Blue";
+            // 
+            // Color_Change
+            // 
+            this.Color_Change.BackColor = System.Drawing.Color.Black;
+            this.Color_Change.Location = new System.Drawing.Point(73, 45);
+            this.Color_Change.Name = "Color_Change";
+            this.Color_Change.ReadOnly = true;
+            this.Color_Change.Size = new System.Drawing.Size(429, 254);
+            this.Color_Change.TabIndex = 2;
+            this.Color_Change.Text = " ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(583, 436);
+            this.Controls.Add(this.Color_Change);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Blue);
+            this.Controls.Add(this.Green);
+            this.Controls.Add(this.Red);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.Red)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Green)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Blue)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar Red;
+        private System.Windows.Forms.TrackBar Green;
+        private System.Windows.Forms.TrackBar Blue;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox Color_Change;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        int A=255, R=0, G=0, B=0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Red_Scroll(object sender, EventArgs e)
+        {
+            R = Red.Value;
+            Color color = Color.FromArgb(A, R, G, B);
+            Color_Change.BackColor = color;
+        }
+
+        private void Green_Scroll(object sender, EventArgs e)
+        {
+            G = Green.Value;
+            Color color = Color.FromArgb(A, R, G, B);
+            Color_Change.BackColor = color;
+        }
+
+        private void Blue_Scroll(object sender, EventArgs e)
+        {
+            B = Blue.Value;
+            Color color = Color.FromArgb(A, R, G, B);
+            Color_Change.BackColor = color;
+        }
+
+       
+    }
+}
